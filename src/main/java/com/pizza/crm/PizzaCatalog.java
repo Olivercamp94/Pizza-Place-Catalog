2f7039940b1537f0cea44f5bb88f19bc3c571e51
Matheus <oc.math.campos@gmail.com>
2025-04-23 13:13:47 -0300

    init: add srp to pizza project (catalog, backend mobile and crm application)

---
+++ src/main/java/com/pizza/crm/PizzaCatalog.java
@@ -0,0 +1,13 @@
+package com.pizza.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCatalog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaCatalog.class, args);
+	}
+
+}
